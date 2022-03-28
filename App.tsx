84bc803785f779c828307b044c9f94bb92435e21
Jesinthan0703 <jesinthan.23cs@licet.ac.in>
2022-03-28 11:16:05 +0530

    updated

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { WebView } from "react-native-webview";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import SafeAreaView from "react-native-safe-area-view";
+import { View } from "react-native";
 
-export default function App() {
+export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} forceInset={{ top: "always" }}>
+        <View style={{ flex: 1 }}>
+          <WebView source={{ uri: "http://192.168.137.106:3000/" }} />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
